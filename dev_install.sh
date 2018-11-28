a96b233c7473b2f71711fd3652a2b6403e20f9bc
Daniel Rodríguez <danielalexis.rodriguez@cencosud.cl>
2018-11-28 09:36:50 -0300

    Cambio de plataforma SCIJava a IJava

--- dev_install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-sjjk=~/anaconda3/envs/java_env/opt/scijava-jupyter-kernel
-
-mvn clean package
-mvn dependency:copy-dependencies
-rm $sjjk/*.jar
-cp target/dependency/*.jar $sjjk/
-cp target/*.jar $sjjk/
-
