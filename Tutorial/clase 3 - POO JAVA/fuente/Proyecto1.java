305b0e470cf49beb12803add6bd859360f027ebb
Daniel Rodríguez <danielalexis.rodriguez@cencosud.cl>
2018-12-03 19:54:09 -0300

    Fuente clase 3

---
+++ Tutorial/clase 3 - POO JAVA/fuente/Proyecto1.java
@@ -0,0 +1,12 @@
+package com.cencosud.sistemas.qa;
+
+public class Proyecto1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String arg[] = {"1", "hola", "pepe" , "lucho"};
+		System.out.println(arg[2]);
+		System.out.println(arg[3]);
+	}
+
+}
